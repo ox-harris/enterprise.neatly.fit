709116f146767a8cd71f579876e2b284225312bd
Oxford Harrison <oxharris.me@gmail.com>
2021-04-07 20:59:08 +0100

    ...

--- server/index.js
+++ server/index.js
@@ -7,22 +7,29 @@ import oauth2 from '@webqit/oauth2-node-client';
 /**
  * Create the OAuth2 middleware
  */
-const oauth2Middleware = oauth2.createMiddleware({
-    clientId:       process.env.OAUTH_CLIENT_ID,
-    clientSecret:   process.env.OAUTH_CLIENT_SECRET,
-    endpoints: {
-        baseUrl:    process.env.OAUTH_ENDPOINT_HOST,
-        login:      process.env.OAUTH_LOGIN_ENDPOINT,
-        token:      process.env.OAUTH_TOKEN_ENDPOINT,
-        logout:     process.env.OAUTH_LOGOUT_ENDPOINT,
-    },
-    callbacks: {
-        baseUrl:    process.env.OAUTH_CALLBACK_HOST,
-        login:      process.env.OAUTH_LOGIN_CALLBACK,
-        logout:     process.env.OAUTH_LOGOUT_CALLBACK,
-    },
-    sesskey:        process.env.OAUTH_SESSION_KEY,
-});
+var oauth2Middleware;
+const getOauth2Middleware = env => {
+    if (!oauth2Middleware) {
+        oauth2Middleware = oauth2.createMiddleware({
+            clientId:       env.OAUTH_CLIENT_ID,
+            clientSecret:   env.OAUTH_CLIENT_SECRET,
+            endpoints: {
+                baseUrl:    env.OAUTH_ENDPOINT_HOST,
+                login:      env.OAUTH_LOGIN_ENDPOINT,
+                token:      env.OAUTH_TOKEN_ENDPOINT,
+                logout:     env.OAUTH_LOGOUT_ENDPOINT,
+            },
+            callbacks: {
+                baseUrl:    env.OAUTH_CALLBACK_HOST,
+                login:      env.OAUTH_LOGIN_CALLBACK,
+                logout:     env.OAUTH_LOGOUT_CALLBACK,
+            },
+            sesskey:        env.OAUTH_SESSION_KEY,
+        });
+    }
+    return oauth2Middleware;
+};
+
 
 /**
  * 
@@ -31,7 +38,7 @@ const oauth2Middleware = oauth2.createMiddleware({
  * @param Function  next 
  */
 export default async function(request, recieved, next) {
-    return this.walk(oauth2Middleware).then(async () => {
+    return this.walk(getOauth2Middleware(this.env)).then(async () => {
         var auth = { oauth: request.oauth, };
         if (next.pathname) {
             return next(auth);
